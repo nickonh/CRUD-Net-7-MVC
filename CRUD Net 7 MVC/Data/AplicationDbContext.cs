db13a326315dc59047c09bbc9f13a09b13dbce8b
NicolasKL <nickonh.arenas@gmail.com>
2023-03-07 19:17:48 -0300

    Agregar archivos de proyecto.

---
+++ CRUD Net 7 MVC/Data/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUD_Net_7_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Net_7_MVC.Data
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //Se agregan los Modelos de la APP
+
+        public DbSet<Contacto> Contactos { get; set; }
+
+    }
+}
---
+++ CRUD Net 7 MVC/Program.cs
@@ -0,0 +1,29 @@
+using CRUD_Net_7_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+//Configuracion del ConnectionString
+builder.Services.AddDbContext<AplicationDbContext>(opciones =>
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQL")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
